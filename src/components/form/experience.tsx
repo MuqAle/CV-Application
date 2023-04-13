b1da6526111b5c7c4584ce77b768280883cf42cc
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-04-13 11:47:15 -0400

    change order

--- src/components/form/experience.tsx
+++ src/components/form/experience.tsx
@@ -13,9 +13,9 @@ const ExperienceItem = ({changeItem,deleteItem,addItem}:ExperienceProp) => {
         <section className="experience">
             <Input  name='position' label='Position' onChange={changeItem}></Input>
             <Input  name='company' label='Company' onChange={changeItem}></Input>
-            <Input  name='location' label='Location' onChange={changeItem}></Input>
             <Input  name='startDate' label='Start Date' onChange={changeItem}></Input>
             <Input  name='endDate' label='End Date' onChange={changeItem}></Input>
+            <Input  name='location' label='Location' onChange={changeItem}></Input>
             <textarea name='description' placeholder= 'Description' onChange={changeItem}></textarea>
             <div className="buttons">
                 <button className="experience_delete" onClick={deleteItem}>Delete</button>
